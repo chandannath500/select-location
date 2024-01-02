f898ca9c6140d66df8d2a4c9a544ff7a93dfccf6
Chandan Nath <46443346+chandannath500@users.noreply.github.com>
2024-01-02 17:25:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import CitySelector from "./components/CitySelector";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CitySelector />
+    </div>
+  );
+}
